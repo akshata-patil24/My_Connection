431eee4341b7916be14ba862a60148f3e917e07a
akshata-patil24 <akshata.patil@dataentrega.com>
2023-10-12 15:02:53 +0530

    Add sample file

---
+++ ConnectToGitHub.ps1
@@ -0,0 +1,11 @@
+﻿# Set your GitHub username and repository details
+$githubUsername = "akshata-patil24"
+$repositoryName = "first_streamlit_app"
+
+# Clone the repository using SSH
+git clone git clone https://github.com/akshata-patil24/first_streamlit_app.git
+
+#git@github.com:akshata-patil24/Connection.git
+
+# Change to the repository directory
+cd $repositoryName
---
+++ CreateSampleFileOnGithub.ps1
@@ -0,0 +1,14 @@
+﻿# Create a sample file
+New-Item -Path "sample.txt" -ItemType "file" -Value "Hello, GitHub!"
+
+# Add and commit changes
+git add .
+git commit -m "Add sample file"
+
+# Push changes to GitHub
+git push origin master
+
+---------------------------------------------------------
+
+# Pull changes from GitHub
+git pull origin master
